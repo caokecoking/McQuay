b58be0f9f3b74a6ec30ab1a6ba687672335d3d53
ck <935259824@qq.com>
2018-11-27 21:56:45 +0800

    ck 3

--- src/main/java/com/zking/controller/xtgl/CompanyControlller.java
+++ src/main/java/com/zking/controller/xtgl/CompanyControlller.java
@@ -1,5 +1,7 @@
 package com.zking.controller.xtgl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zking.mapper.PingYing;
 import com.zking.pojo.xtgl.Company;
 import com.zking.service.xtgl.ICompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -44,11 +48,71 @@ public class CompanyControlller {
         return map;
     }
 
+
+    @ResponseBody
     @RequestMapping("addCompany")
-    public String addCompany(Company company){
-        return "xtgl/CompanyAdd";
-//        if(ics.add(company)>0){
-//            return "xtgl/CompanyList1";
-//        }
+    public Map<String,Object> addCompany(Company company){
+        Map<String,Object> map=new HashMap<>();
+        String max = String.valueOf(ics.getEnd(company));
+        if (max == null || max.equals("null")) {
+            max = "1";
+        }
+        System.out.println(company.getProv());
+        String Coding = PingYing.getFirstLetter(company.getProv());
+        if (max.length() == 1) {
+            Coding += "000" + max;
+        } else if (max.length() == 2) {
+            Coding += "00" + max;
+        } else if (max.length() == 3) {
+            Coding += "0" + max;
+        } else if (max.length() == 4) {
+            Coding += max;
+        }
+        company.setCompCoding(Coding);
+        int index=ics.add(company);
+        map.put("msg",index);
+        return map;
+    }
+
+    @ResponseBody
+    @RequestMapping("editCompany")
+    public Map<String,Object> editCompany(Company company){
+        Map<String,Object> map=new HashMap<>();
+        String max = String.valueOf(ics.getEnd(company));
+        if (max == null || max.equals("null")) {
+            max = "1";
+        }
+        String Coding = PingYing.getFirstLetter(company.getProv());
+        if (max.length() == 1) {
+            Coding += "000" + max;
+        } else if (max.length() == 2) {
+            Coding += "00" + max;
+        } else if (max.length() == 3) {
+            Coding += "0" + max;
+        } else if (max.length() == 4) {
+            Coding += max;
+        }
+        company.setCompCoding(Coding);
+        int index=ics.edit(company);
+        map.put("msg",index);
+        return map;
+    }
+
+    @ResponseBody
+    @RequestMapping("findOnlyCompany1")
+    public Map findOnlyCompany1(Company company){
+        Map<String,Object> map=new HashMap<>();
+        Company company1=ics.findonly(company);
+        map.put("company",company1);
+        return map;
+    }
+
+    @ResponseBody
+    @RequestMapping("findOnlyCompany2")
+    public Map findOnlyCompany2(Company company){
+        Map<String,Object> map=new HashMap<>();
+        Company company1=ics.findonly(company);
+        map.put("company",company1);
+        return map;
     }
 }
